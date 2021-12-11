fb04d6f606efb31157a4f121a416987414f9b6ae
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-12-11 11:10:23 +0700

    add footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,6 +1,7 @@
 import { useSelector, shallowEqual } from 'react-redux'
 
 import moment from 'moment'
+import { Img } from 'react-image'
 import { FaHeart } from 'react-icons/fa'
 
 export default function Footer() {
@@ -8,12 +9,21 @@ export default function Footer() {
   const { theme } = { ...preferences }
 
   return (
-    <div className={`footer flex flex-col md:flex-row items-center text-xs font-light p-3 ${theme}`}>
-      <span className="hidden sm:block w-full md:w-1/3 flex items-center justify-center md:justify-start text-gray-400 mt-4 md:mt-0">
+    <div className={`footer flex flex-col md:flex-row items-center text-xs font-light space-y-2 sm:space-y-0 p-3 ${theme}`}>
+      <span className="w-full md:w-1/3 flex items-center justify-center md:justify-start text-gray-400">
+        <div className="flex items-center text-gray-600 dark:text-white font-medium space-x-0">
+          <span>Built with</span>
+          <Img
+            src="/logos/connext/logo.png"
+            alt=""
+            className="w-6 h-6 rounded-full"
+          />
+          <span>Connext Protocol</span>
+        </div>
       </span>
-      <span className="hidden sm:block w-full md:w-1/3 flex items-center justify-center text-gray-400 mt-4 md:mt-0">
+      <span className="hidden md:block w-full md:w-1/3 flex items-center justify-center text-gray-400">
       </span>
-      <span className="w-full md:w-1/3 flex items-center justify-center md:justify-end text-gray-400 space-x-1 mt-0 md:mt-0">
+      <span className="w-full md:w-1/3 flex items-center justify-center md:justify-end text-gray-400 space-x-1">
         <span>© {moment().format('YYYY')} made with</span>
         <FaHeart className="text-red-400 text-xl" />
         <span>by <span className="font-semibold">{process.env.NEXT_PUBLIC_TEAM_NAME}</span> team.</span>
