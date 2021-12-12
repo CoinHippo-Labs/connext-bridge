ce6cb6f6a0fc318e4c2f04278dfc2649871cd4c2
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-12-13 02:14:35 +0700

    add config

---
+++ config/assets.js
@@ -0,0 +1,217 @@
+[
+  {
+    "id": "usdt",
+    "symbol": "USDT",
+    "contracts": [
+      {
+        "contract_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        "chain_id": 1,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x55d398326f99059ff775485246999027b3197955",
+        "chain_id": 56,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+        "chain_id": 137,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+        "chain_id": 42161,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58",
+        "chain_id": 10,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xc7198437980c041c805a1edcba50c1ce5db95118",
+        "chain_id": 43114,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x049d68029688eabf473097a2fc38ef61633a3c7a",
+        "chain_id": 250,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x4ecaba5870353805a9f068101a40e0f32ed605c6",
+        "chain_id": 100,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xb44a9b6905af7c801311e8f4e76932ee959c663c",
+        "chain_id": 1285,
+        "contract_decimals": 6,
+      }
+    ]
+  },
+  {
+    "id": "usdc",
+    "symbol": "USDC",
+    "contracts": [
+      {
+        "contract_address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        "chain_id": 1,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+        "chain_id": 56,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+        "chain_id": 137,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+        "chain_id": 42161,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+        "chain_id": 10,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664",
+        "chain_id": 43114,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0x04068da6c83afcfa0e13ba15a6696662335d5b75",
+        "chain_id": 250,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83",
+        "chain_id": 100,
+        "contract_decimals": 6,
+      },
+      {
+        "contract_address": "0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d",
+        "chain_id": 1285,
+        "contract_decimals": 6,
+      }
+    ]
+  },
+  {
+    "id": "dai",
+    "symbol": "DAI",
+    "contracts": [
+      {
+        "contract_address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+        "chain_id": 1,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3",
+        "chain_id": 56,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+        "chain_id": 137,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+        "chain_id": 42161,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+        "chain_id": 10,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0xd586e7f844cea2f87f50152665bcbc2c279d8d70",
+        "chain_id": 43114,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e",
+        "chain_id": 250,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x0000000000000000000000000000000000000000",
+        "chain_id": 100,
+        "contract_decimals": 18,
+      }
+    ]
+  },
+  {
+    "id": "eth",
+    "symbol": "ETH",
+    "contracts": [
+      {
+        "contract_address": "0x0000000000000000000000000000000000000000",
+        "chain_id": 1,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
+        "chain_id": 56,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+        "chain_id": 137,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x0000000000000000000000000000000000000000",
+        "chain_id": 42161,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x0000000000000000000000000000000000000000",
+        "chain_id": 10,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab",
+        "chain_id": 43114,
+        "contract_decimals": 18,
+      },
+      {
+        "contract_address": "0x74b23882a30290451a17c44f4f05243b6b58c76d",
+        "chain_id": 250,
+        "contract_decimals": 18,
+      }
+    ]
+  },
+  {
+    "id": "wbtc",
+    "symbol": "WBTC",
+    "contracts": [
+      {
+        "contract_address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+        "chain_id": 1,
+        "contract_decimals": 8,
+      },
+      {
+        "contract_address": "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
+        "chain_id": 137,
+        "contract_decimals": 8,
+      },
+      {
+        "contract_address": "0x50b7545627a5162f82a992c33b87adc75187b218",
+        "chain_id": 43114,
+        "contract_decimals": 8,
+      },
+      {
+        "contract_address": "0x321162cd933e2be498cd2267a90534a804051b11",
+        "chain_id": 250,
+        "contract_decimals": 8,
+      }
+    ]
+  }
+]
