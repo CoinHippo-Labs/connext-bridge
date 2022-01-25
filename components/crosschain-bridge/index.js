3db78df79386b146f1dc815d137ff3e7ed3e2031
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-01-25 14:51:13 +0700

    change message, nomad to prod

--- components/crosschain-bridge/index.js
+++ components/crosschain-bridge/index.js
@@ -1140,7 +1140,7 @@ export default function CrosschainBridge() {
     </>
   )
 
-  const supportNomad = (process.env.NEXT_PUBLIC_NETWORK || process.env.NEXT_PUBLIC_SITE_URL?.includes('staging')) && swapConfig.fromChainId && swapConfig.toChainId && fromAsset?.nomad_support?.findIndex(pair => pair?.from_chain_id === swapConfig.fromChainId && pair?.to_chain_id === swapConfig.toChainId) > -1 && toAsset?.nomad_support?.findIndex(pair => pair?.from_chain_id === swapConfig.fromChainId && pair?.to_chain_id === swapConfig.toChainId) > -1
+  const supportNomad = (true || process.env.NEXT_PUBLIC_NETWORK || process.env.NEXT_PUBLIC_SITE_URL?.includes('staging')) && swapConfig.fromChainId && swapConfig.toChainId && fromAsset?.nomad_support?.findIndex(pair => pair?.from_chain_id === swapConfig.fromChainId && pair?.to_chain_id === swapConfig.toChainId) > -1 && toAsset?.nomad_support?.findIndex(pair => pair?.from_chain_id === swapConfig.fromChainId && pair?.to_chain_id === swapConfig.toChainId) > -1
   const useNomad = supportNomad && bridgeProtocol === 'nomad'
   const nomadUrl = useNomad && (fromChain.optional_bridge_urls?.find(url => url?.includes('.nomad.')) || toChain.optional_bridge_urls?.find(url => url?.includes('.nomad.')))
   receivingAddress = useNomad ? advancedOptions?.receiving_address || address : receivingAddress
@@ -2311,7 +2311,7 @@ export default function CrosschainBridge() {
                                         rounded={true}
                                       >
                                         <div className="flex items-center justify-between space-x-1">
-                                          <span className="font-mono text-sm">Connecting to the protocol, please wait for seconds and reinsert the amount. If this message persists, please refresh the page.</span>
+                                          <span className="font-mono text-sm">Please wait a few seconds and refresh{/*Connecting to the protocol, please wait for seconds and reinsert the amount. If this message persists, please refresh the page.*/}</span>
                                           <button
                                             onClick={() => setEstimateTrigger(moment().valueOf())}
                                             className="bg-blue-500 dark:bg-blue-500 flex items-center justify-center text-white rounded-full p-2"
