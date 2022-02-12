cd2f872fcfaa27bc3f601b5cba9b7b1cb35468e2
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-02-12 14:07:31 +0700

    get version from package

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,13 +4,15 @@ import moment from 'moment'
 import { Img } from 'react-image'
 import { FaHeart } from 'react-icons/fa'
 
+import _package from '../../package.json'
+
 export default function Footer() {
   const { preferences } = useSelector(state => ({ preferences: state.preferences }), shallowEqual)
   const { theme } = { ...preferences }
 
   return (
     <div className={`footer flex flex-col md:flex-row items-center text-xs font-light space-y-2 sm:space-y-0 p-3 ${theme}`}>
-      <span className="w-full md:w-1/3 flex items-center justify-center md:justify-start text-gray-400">
+      <span className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center md:justify-start text-gray-400">
         <div className="flex items-center text-gray-600 dark:text-white font-medium space-x-1.5">
           <span>Built with</span>
           <Img
@@ -19,14 +21,14 @@ export default function Footer() {
             className="w-4 h-4 rounded-full"
           />
           <span>Connext Protocol</span>
-          {process.env.NEXT_PUBLIC_SDK_VERSION && (
-            <span className="font-mono text-gray-400 dark:text-gray-600 font-light">(SDK v{process.env.NEXT_PUBLIC_SDK_VERSION})</span>
+          {_package?.dependencies?.['@connext/nxtp-sdk'] && (
+            <span className="font-mono text-gray-400 dark:text-gray-600 font-light">(SDK v{_package.dependencies['@connext/nxtp-sdk'].replace('^', '')})</span>
           )}
         </div>
       </span>
-      <span className="hidden md:block w-full md:w-1/3 flex items-center justify-center text-gray-400">
+      <span className="hidden lg:flex w-full lg:w-1/3 items-center justify-center text-gray-400">
       </span>
-      <span className="w-full md:w-1/3 flex items-center justify-center md:justify-end text-gray-400 space-x-1">
+      <span className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center md:justify-end text-gray-400 space-x-1">
         <span>© {moment().format('YYYY')} made with</span>
         <FaHeart className="text-red-400 text-xl" />
         <span>by <span className="font-semibold">{process.env.NEXT_PUBLIC_TEAM_NAME}</span> team.</span>
