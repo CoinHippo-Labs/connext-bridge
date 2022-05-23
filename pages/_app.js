81b86ac4ff2d841fa67883ec9de273cb8931a1ab
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-05-24 01:59:09 +0700

    update script

--- pages/_app.js
+++ pages/_app.js
@@ -61,7 +61,7 @@ export default function App({ Component, pageProps }) {
                   (function() {
                     var u="https://connextnetwork.matomo.cloud/";
                     _paq.push(['setTrackerUrl', u+'matomo.php']);
-                    _paq.push(['setSiteId', '2']);
+                    _paq.push(['setSiteId', '4']);
                     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                     g.async=true; g.src='//cdn.matomo.cloud/connextnetwork.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
                   })();
