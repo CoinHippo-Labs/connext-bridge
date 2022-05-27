7aa88696317eeee5a0bfb444efd721259417c81e
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-05-27 13:50:43 +0700

    remove signerAddress on sdk initialize

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -174,7 +174,7 @@ export default function Navbar() {
           type: SDK,
           value: await create({
             chains: chains_config,
-            signerAddress: address,
+            // signerAddress: address,
             logLevel: 'info',
             network: process.env.NEXT_PUBLIC_NETWORK,
             environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
