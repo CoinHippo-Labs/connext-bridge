30fcf278835c301f8d01c6dfb46830c1689e48eb
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-10-12 23:02:20 +0700

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import Router from 'next/router'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import NProgress from 'nprogress'
+import TagManager from 'react-gtm-module'
 
 import Layout from '../layouts'
 import { useStore } from '../store'
+import * as ga from '../lib/ga'
 import '../styles/globals.css'
 import '../styles/animate.css'
 import '../styles/layout.css'
@@ -38,7 +42,31 @@ export default ({
   Component,
   pageProps,
 }) => {
+  const router = useRouter()
   const store = useStore(pageProps.initialReduxState)
+
+  useEffect(() => {
+    const handleRouteChange = url =>
+      ga.pageview(url)
+
+    router.events.on(
+      'routeChangeComplete',
+      handleRouteChange,
+    )
+
+    return () => router.events.off(
+      'routeChangeComplete',
+      handleRouteChange,
+    )
+  }, [router.events])
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_GTM_ID) {
+      TagManager.initialize({
+        gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+      })
+    }
+  }, [])
 
   return (
     <>
@@ -70,16 +98,30 @@ export default ({
           name="theme-color"
           content="#050707"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _mtm = window._mtm = window._mtm || [];
-              _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src='https://cdn.matomo.cloud/connextnetwork.matomo.cloud/container_eMNAaOFI.js'; s.parentNode.insertBefore(g,s);
-            `,
-          }}
-        />
+        {
+          process.env.NEXT_PUBLIC_GA_TRACKING_ID &&
+          (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                      });
+                    `,
+                  }}
+                />
+            </>
+          )
+        }
       </Head>
       <Provider store={store}>
         <Layout>
