571868180802fa2482996b1e4684c449e4b3211b
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-11-21 11:06:10 +0700

    styling

--- components/swap/options/index.js
+++ components/swap/options/index.js
@@ -75,7 +75,7 @@ export default ({
     <Modal
       disabled={disabled}
       buttonTitle={
-        <div className={`bg-slate-50 hover:bg-slate-100 dark:bg-slate-900 dark:hover:bg-slate-800 rounded-lg flex items-center ${applied ? 'text-blue-400 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400' : 'text-slate-500 hover:text-slate-600 dark:text-slate-300 dark:hover:text-slate-200'} space-x-1 py-2 px-3`}>
+        <div className={`bg-slate-50 hover:bg-slate-100 dark:bg-slate-900 dark:hover:bg-slate-800 rounded-lg flex items-center ${applied ? 'text-blue-400 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400' : 'text-slate-500 hover:text-slate-600 dark:text-slate-300 dark:hover:text-slate-200'} space-x-1 py-2 px-2`}>
           <RiSettings3Line
             size={20}
           />
