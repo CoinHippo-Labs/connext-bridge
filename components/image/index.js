985aeef5a337075f8c92db04525c966f281bb441
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-01-27 02:44:12 +0700

    feat: remove image optimizer url

--- components/image/index.js
+++ components/image/index.js
@@ -4,26 +4,31 @@ const loader = (
   {
     src,
     width,
-    quality,
+    quality = 75,
   },
 ) =>
-  `${process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL}/_next${
+  `${
+    process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL ?
+      `${process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL}/_next` :
+      ''
+  }${
     src?.startsWith('/') ?
       '' :
       '/'
-  }${src}?url=${
-    src?.startsWith('/') ?
-      process.env.NEXT_PUBLIC_SITE_URL :
+  }${src}${
+    process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL ?
+      `?url=${
+        src?.startsWith('/') ?
+          process.env.NEXT_PUBLIC_SITE_URL :
+          ''
+      }${src}&w=${width}&q=${quality}` :
       ''
-  }${src}&w=${width}&q=${
-    quality ||
-    75
   }`
 
 export default (
   {
     ...rest
-  },
+  }
 ) => {
   return (
     <Image
