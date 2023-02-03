cfabeae1f64bd3e1d3e9419b5eb61c8f09b64ef5
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-02-03 13:28:34 +0700

    chore: update vecel deployment script

--- vercel.deployment.sh
+++ vercel.deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] && ! [[ "$VERCEL_URL" == *"v1"* ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
