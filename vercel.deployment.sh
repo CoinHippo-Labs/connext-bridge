f0210977166defbeec7c267fa012301e574ef4c5
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-03-09 10:52:12 +0900

    update deploy script

--- vercel.deployment.sh
+++ vercel.deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ] || [ "$VERCEL_GIT_COMMIT_REF" == "feat/*" ]] && ! [[ "$VERCEL_URL" == *"v1"* ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ] || [ "$VERCEL_GIT_COMMIT_REF" == *"feat/"* ]] && ! [[ "$VERCEL_URL" == *"v1"* ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
