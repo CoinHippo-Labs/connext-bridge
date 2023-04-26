8afe98e3639198e0770663f68cad8ca501ae8790
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-04-27 00:11:43 +0700

    chore: change url

--- components/agree-to-terms/index.js
+++ components/agree-to-terms/index.js
@@ -66,7 +66,7 @@ export default (
                 and
               </span>
               <a
-                href={process.env.NEXT_PUBLIC_DOCS_URL}
+                href={`${process.env.NEXT_PUBLIC_DOCS_URL}/routers/Guides/security`}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="underline text-black dark:text-white font-semibold"
