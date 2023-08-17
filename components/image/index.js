d1147943e9ca5f3b6c9a7c69435f15d9c678acca
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-08-17 12:36:23 +0700

    chore: handle image change

--- components/image/index.js
+++ components/image/index.js
@@ -5,50 +5,34 @@ const IMAGE_OPTIMIZER_URL = ''
 const loader = ({ src, width, quality = 75 }) => `${IMAGE_OPTIMIZER_URL ? `${IMAGE_OPTIMIZER_URL}/_next` : ''}${src?.startsWith('/') ? '' : '/'}${src}${IMAGE_OPTIMIZER_URL ? `?url=${src?.startsWith('/') ? process.env.NEXT_PUBLIC_APP_URL : ''}${src}&w=${width}&q=${quality}` : ''}`
 
 export default ({ src, srcEnd, duration = 2, alt = '', ...rest }) => {
-  const [imageSrc, setImageSrc] = useState(src)
-  const [timer, setTimer] = useState(0)
+  const [timer, setTimer] = useState(null)
 
   useEffect(
     () => {
-      const timeout = setTimeout(() => setTimer(timer + 1), 1 * 1000)
-      return () => clearTimeout(timeout)
+      if (typeof timer === 'number') {
+        const timeout = setTimeout(() => setTimer(timer + 1), 1 * 1000)
+        return () => clearTimeout(timeout)
+      }
     },
     [timer],
   )
 
   useEffect(
     () => {
-      if (src && (!srcEnd || timer === 0)) {
-        setImageSrc(src)
-      }
-    },
-    [src, timer],
-  )
-
-  useEffect(
-    () => {
-      if (srcEnd && timer > duration) {
-        setImageSrc(srcEnd)
-      }
-    },
-    [srcEnd, timer],
-  )
-
-  useEffect(
-    () => {
-      if (src && srcEnd && timer > 0) {
+      if (src && srcEnd) {
         setTimer(0)
       }
     },
     [src, srcEnd],
   )
 
+  const _src = timer >= duration && srcEnd ? srcEnd : src
   return (
     <Image
       alt={alt}
       { ...rest }
-      src={imageSrc}
-      loader={() => loader({ ...rest, src: imageSrc })}
+      src={_src}
+      loader={() => loader({ ...rest, src: _src })}
       unoptimized={true}
     />
   )
