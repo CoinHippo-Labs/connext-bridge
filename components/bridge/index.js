206d18ebe15192801b6de051071c5ab1ea95d4d4
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-11-16 12:32:41 +0700

    chore: handle next asset on source to destination native chain

--- components/bridge/index.js
+++ components/bridge/index.js
@@ -1335,6 +1335,7 @@ export default ({ useAssetChain = false }) => {
       destination_contract_data = { ...destination_contract_data, contract_address: ZeroAddress, symbol: destination_asset_data.symbol, image: destination_asset_data.image }
     }
   }
+  const isNextAssetOnSource = equalsIgnoreCase(source_contract_data?.contract_address, _source_contract_data?.next_asset?.contract_address)
   const isNextAssetOnDestination = equalsIgnoreCase(destination_contract_data?.contract_address, _destination_contract_data?.next_asset?.contract_address)
 
   const source_symbol = source_contract_data?.symbol || source_asset_data?.symbol
@@ -2191,7 +2192,7 @@ export default ({ useAssetChain = false }) => {
                                     </div>
                                   </div>
                                 )}
-                                {!is_xERC20 && !isNextAssetOnDestination && (
+                                {!is_xERC20 && !isNextAssetOnDestination && !(isNextAssetOnSource && !_destination_contract_data?.next_asset) && (
                                   <>
                                     {source !== 'pool' && (
                                       <div className="flex flex-col space-y-0.5">
