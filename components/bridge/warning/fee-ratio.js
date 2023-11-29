612f1c787d020f61f36dfc58f842abf02da2e522
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-11-29 18:43:52 +0700

    feat: not show warning when fee : amount >= 1

--- components/bridge/warning/fee-ratio.js
+++ components/bridge/warning/fee-ratio.js
@@ -3,7 +3,7 @@ import { IoWarning } from 'react-icons/io5'
 const RATIO_THRESHOLD = 0.5
 
 export default ({ ratio = 0 }) => {
-  return Number(ratio) > RATIO_THRESHOLD && (
+  return Number(ratio) > RATIO_THRESHOLD && Number(ratio) < 1 && (
     <div className="flex items-start space-x-1.5 3xl:space-x-2.5">
       <IoWarning size={16} className="min-w-max 3xl:w-5 3xl:h-5 text-yellow-500 dark:text-yellow-400" />
       <div className="text-yellow-500 dark:text-yellow-400 text-xs 3xl:text-xl">
